ea0078b06a9f963ae2caa0455dcda2ffde2702d7
Stayrony <ana.mizumskaya@gmail.com>
2016-10-26 04:50:51 +0300

    develop login logic

---
+++ XamarinSample/XamarinSample/ViewModels/LoginViewModel.cs
@@ -0,0 +1,101 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoginViewModel.cs" company="">
+// </copyright>
+// <summary>
+//   The login view model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace XamarinSample.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+
+    using XamarinSample.Contract;
+    using XamarinSample.Models;
+
+    /// <summary>
+    /// The login view model.
+    /// </summary>
+    public class LoginViewModel : BaseViewModel
+    {
+        private string login;
+
+        private string password;
+
+        private Command loginCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Login
+        {
+            get { return this.login; }
+            set
+            {
+                if (this.login != value)
+                {
+                    this.login = value;
+                    this.OnPropertyChanged("Login");
+                }
+            }
+        }
+
+        public Command LoginCommand
+        {
+            get { return this.loginCommand ?? (this.loginCommand = new Command(async () => await this.ExecuteLoginCommand())); }
+
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set
+            {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    this.OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            // if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task ExecuteLoginCommand()
+        {
+            var error = false;
+            try
+            {
+                Member memberLoginInfo = new Member { Login = this.Login, Password = this.Password };
+
+                bool result =  DependencyService.Get<IAuthorizationService>().Login(memberLoginInfo);
+                if (result)
+                {
+                    var page = new ContentPage();
+                    await page.DisplayAlert("Success", "Success log in.", "OK");
+                }
+                else
+                {
+                    var page = new ContentPage();
+                    await page.DisplayAlert("Login failed", "User cannot log in.", "OK");
+                }
+            }
+            catch (Exception)
+            {
+                error = true;
+            }
+
+            if (error)
+            {
+                var page = new ContentPage();
+                await page.DisplayAlert("Error", "User cannot log in.", "OK");
+            }
+        }
+    }
+}
--- XamarinSample/XamarinSample/Views/LoginPage.xaml.cs
+++ XamarinSample/XamarinSample/Views/LoginPage.xaml.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoginPage.xaml.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The login page.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace XamarinSample.Views
 {
+    using Xamarin.Forms;
+
+    using XamarinSample.ViewModels;
+
+    /// <summary>
+    /// The login page.
+    /// </summary>
     public partial class LoginPage : ContentPage
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginPage"/> class.
+        /// </summary>
         public LoginPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.BindingContext = new LoginViewModel();
         }
     }
 }
